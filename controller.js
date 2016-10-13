10f3ba740e7a59e0e552a3c53a5459e38c3bf6ee
Chad Rohr <chadrohr@yahoo.com>
2016-10-13 13:18:07 -0600

    complete switch to items like weather app

---
+++ controller.js
@@ -0,0 +1,18 @@
+this.photoOfTheDay = function(data) {
+    
+    var baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=';
+    var key = 'LW8jHnw3EjJ9JiBkdMxt2HRtbkTw4x1xXwe2AAt6';
+    var url = baseUrl + key;
+
+
+    return new Promise(function (resolve, reject) {
+        $.get(url).then(
+            function (data) {
+                resolve(data);
+            },
+            function (error) {
+                reject(error);
+            }
+        );
+    })
+}
---
+++ nasa-service.js
@@ -0,0 +1,15 @@
+var nasaService = new NasaService();
+
+
+nasaService.photoOfTheDay()
+    .then(function (data) {
+        var template = $('#nasaTemplate').html();
+
+        template = template.replace('{{ url }}', data.url)
+        template = template.replace('{{ title }}', data.title)
+        template = template.replace('{{ copyright }}', data.copyright)
+        template = template.replace('{{ date }}', data.date)
+        template = template.replace('{{ explanation }}', data.explanation)
+
+        $('#myTemplate').append($(template));
+    })
